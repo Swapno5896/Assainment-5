8e2d40bc3ffa3bf5600a8a82cd8a9ef4e56b7af4
swapno <Swapno5896@yahoo.com>
2021-02-07 22:05:55 +0600

    done

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const fetcha=(link,inputValue)=>{
             container.innerHTML =null
             data.meals.forEach(item => {
                 
-                console.log(item);
+             
                 const newDiv = document.createElement('div')
                 newDiv.className='newDiv'
                 const hemlTem = `
@@ -43,32 +43,33 @@ const fetcha=(link,inputValue)=>{
   })
 
 }
-  
 
-
-
-    
+   
 const detail =id=>{
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then(res=>res.json())
     .then(data=>{
+       
     const item = data.meals[0];
     const detail = document.getElementById('detail')
     const newDetail = document.createElement('div')
+    document.getElementById('detail').innerHTML=null
     const htmlTem = `
                     <img src="${item.strMealThumb}" alt="">
                     <h2>${item.strMeal}</h2>
                     <h3>Ingredients</h3>
                     <ul id="ul"></ul> 
-                      `
+                      `  
     newDetail.innerHTML= htmlTem;
     detail.appendChild(newDetail)
     for (let i = 1; i < 15; i++) {
         let element = i;
         let ingredients =item['strIngredient'+i]
+       
         if(!(ingredients==null || ingredients=='')){
             const ul = document.getElementById('ul')
             const li =  document.createElement('li')
+            console.log(ingredients);
             li.innerText= ingredients
             ul.appendChild(li)  
         }         

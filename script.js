7cdcf2214611eaefbab8ff553d3a774976575af8
swapno <Swapno5896@yahoo.com>
2021-02-07 20:33:58 +0600

    f

--- script.js
+++ script.js
@@ -18,48 +18,53 @@ const startSearch =()=>{
 const fetcha=(link,inputValue)=>{
     fetch(fetchLink)
     .then(res=>res.json())
-    .then(Error=>alert(`Sorry, We don't have ${inputValue}`))
-    .then(data=>{ 
-    for(let i =0; i <data.meals.length; i++){
-        const value = data.meals[i]
-        const newDiv = document.createElement('div')
-        newDiv.className='newDiv'
-        const hemlTem = `
-        <div onclick="detail(${value.idMeal})">
-        <img src="${value.strMealThumb}" alt="">
-        <h3>${value.strMeal}</h3>
-  
-        </div>`
-        newDiv.innerHTML = hemlTem;
-        container.appendChild(newDiv)
-        }    
-    })
+    .catch(err=>(alert(`Sorry, We don't have ${inputValue}`)))
+    .then(data=>{
+      data.meals.forEach(item => {
+          const newDiv = document.createElement('div')
+          newDiv.className='newDiv'
+          const hemlTem = `
+          <div onclick="detail(${item.idMeal})">
+          <img src="${item.strMealThumb}" alt="">
+          <h3>${item.strMeal}</h3>
+          </div>`
+          newDiv.innerHTML = hemlTem;
+          container.appendChild(newDiv)
+      });
+  })
 
 }
-   
+  
+
+
+
     
-   
 const detail =id=>{
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
     .then(res=>res.json())
     .then(data=>{
-    const value = data.meals[0]
-    console.log(value);
+    const item = data.meals[0];
     const detail = document.getElementById('detail')
     const newDetail = document.createElement('div')
     const htmlTem = `
-    <img src="${value.strMealThumb}" alt="">
-    <h2>${value.strMeal}</h2>
+    <img src="${item.strMealThumb}" alt="">
+    <h2>${item.strMeal}</h2>
     <h3>Ingredients</h3>
     <ul id="ul"></ul>   `
     newDetail.innerHTML= htmlTem;
     detail.appendChild(newDetail)
-    const ul = document.getElementById('ul')
-    const li =  document.createElement('li')
-    li.innerText= value.strIngredient[i]
-    console.log(value.strIngredient1);
-    ul.appendChild(li)   
-})
-   }
+    for (let i = 1; i < 15; i++) {
+        let element = i;
+    
+        let ingredients =item['strIngredient'+i]
+        if(!(ingredients==null || ingredients=='')){
+            const ul = document.getElementById('ul')
+            const li =  document.createElement('li')
+            li.innerText= ingredients
+            ul.appendChild(li)  
+        }         
+      }
+   })
+}
    
-   
+
